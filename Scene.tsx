be6f946b860ae557298aa317893de6b6e1c5ec9f
Prajjwal Acharya <prajjwalacharya@Prajjwals-MacBook-Air.local>
2025-12-27 14:37:50 +0530

    Extra changes

--- Scene.tsx
+++ Scene.tsx
@@ -32,8 +32,8 @@ export function Scene() {
             </Suspense>
 
             <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -5, 0]} receiveShadow>
-                <planeGeometry args={[10, 10]} />
-                <meshStandardMaterial color="#333333" metalness={1} />
+                <planeGeometry args={[100, 100]} />
+                <shadowMaterial transparent opacity={0.5} />
             </mesh>
         </>
     );
